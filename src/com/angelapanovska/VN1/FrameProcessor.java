704d40f579f0d8fa727965204459f5ba26f325df
anpan7088 <anpan7088@gmail.com>
2024-12-22 13:36:46 +0100

    Small bug fixed with the file path

--- src/com/angelapanovska/VN1/FrameProcessor.java
+++ src/com/angelapanovska/VN1/FrameProcessor.java
@@ -3,7 +3,6 @@ package com.angelapanovska.VN1;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.BufferedInputStream;
 import java.io.File;
 import java.util.Arrays;
 

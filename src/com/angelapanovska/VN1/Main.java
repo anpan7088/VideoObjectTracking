897d9a577187ad70d2b889c7a9f2ef4322fc3c41
anpan7088 <anpan7088@gmail.com>
2025-01-19 13:16:00 +0100

    Comments added.

--- src/com/angelapanovska/VN1/Main.java
+++ src/com/angelapanovska/VN1/Main.java
@@ -1,3 +1,16 @@
+/*89211031 - Angela Panovska
+ * Sequential part: 
+ * 1. Extracted frames from the penguin video(could be any video) with the given app FFmpeg.
+ * 2. Sucessfully, extracting the new frames in new folder where its going to be used in the future with this command: 
+ * ffmpeg -i penguins.mp4 -vf fps=1 -q:v 2 frames/frame_%04d.png to make the final video.
+ * 3. Wrote an alghoritm to track the objects with any movements, all pixels are colored in yellow color and saved in new folder.
+ * 4. Next step is the algorithm to draw the bounding boxes around the objects (rectangles in different colors) whenever I put or try anything with that algorithm 
+ * the frames don't extract in the new folder no more, don't know why, I'm trying to fix it but I don't know how. I've been working on it for almost a week on it
+ * should've contacted for help, but I thought I will fix it, my mistake I apologize for that.
+ * 
+ * On my Github is my  other branch "Main" where its without the bounding boxes and it works detecting movements of the objects.
+ * Github: https://github.com/anpan7088/VideoObjectTracking/tree/Main
+ */
 package com.angelapanovska.VN1;
 
 import java.io.File;

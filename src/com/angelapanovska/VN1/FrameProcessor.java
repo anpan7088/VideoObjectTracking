d0aea8ba8f0d2f39b38e6adec3f5a9f364bc0398
anpan7088 <anpan7088@gmail.com>
2025-07-25 12:39:16 +0200

    Threshold value: 100 on second video

--- src/com/angelapanovska/VN1/FrameProcessor.java
+++ src/com/angelapanovska/VN1/FrameProcessor.java
@@ -47,7 +47,7 @@ public class FrameProcessor {
         int width = frame.getWidth();
         int height = frame.getHeight();
         boolean[][] diff = new boolean[width][height];
-        int threshold = 10;
+        int threshold = 100;
 
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
